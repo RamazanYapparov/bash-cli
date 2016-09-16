c84e3ee95c258eb939933de826e4c229b3423306
ramazan.yapparov <ramazan20082@yandex.ru>
2016-09-16 17:06:05 +0300

    Added page fetching

--- main/quotes.py
+++ main/quotes.py
@@ -4,8 +4,21 @@ import re
 
 
 class BashQuotes:
-
     URL = 'bash.im'
+    g = Grab(url=URL)
+    g.go(URL)
+
+    def _get_current_page(self):
+        bash = self.g.doc.select('.//input[@class="page"]').html()
+        page = re.findall('max="\d*"', bash)
+        page = re.sub('\D', '', str(page))
+        print(page)
+        return int(page)
+
+    def _get_quotes_from_specified_page(self, page):
+        self.g.go(self.URL + '/index/' + str(page))
+        bash = self.g.doc.select('.//div[@class="quote"]')
+        return self._get_quotes(bash)
 
     @staticmethod
     def _get_quotes(bash):
@@ -25,11 +38,31 @@ class BashQuotes:
                 pass
         return quotes_dict
 
+    # last_page = None
+    # pages_list = None
+
+    def __init__(self):
+        self.last_page = self._get_current_page()
+        self.pages_list = [k for k in range(self.last_page)]
+
     def get_new_quotes(self):
-        g = Grab(url=self.URL)
-        g.go("bash.im")
-        bash = g.doc.select('.//div[@class="quote"]')
-        return self._get_quotes(bash)
+        return self._get_quotes_from_specified_page(self.last_page)
+
+    def get_next_page(self, page):
+        cur_index = self.pages_list.index(page)
+        try:
+            page = self.pages_list[cur_index + 1]
+            return self._get_quotes_from_specified_page(page)
+        except IndexError:
+            pass
+
+    def get_prev_page(self, page):
+        cur_index = self.pages_list.index(page)
+        try:
+            page = self.pages_list[cur_index - 1]
+            return self._get_quotes_from_specified_page(page)
+        except IndexError:
+            pass
 
     def get_random_quotes(self):
         g = Grab(url=self.URL + '/random')
