e71b7f8dc83c62758f8ae67ba9e7ab73f1e5d5d7
ramazan.yapparov <ramazan20082@yandex.ru>
2016-09-17 10:32:03 +0300

    Added proper paging

--- main/cli.py
+++ main/cli.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3.5
 import urwid
 import quotes
+
 # import main.quotes as quotes
 
 choices = "Новые Случайные".split()
+
+bash = quotes.BashQuotes()
+
+cur_page = bash.last_page
 
 
 def menu(title='Bash.im', choices="Новые Случайные".split()):
@@ -16,40 +21,61 @@ def menu(title='Bash.im', choices="Новые Случайные".split()):
 
 
 def show_new():
-    quotes_list = quotes.BashQuotes().get_new_quotes()
+    quotes_list = bash.get_new_quotes()
     return get_quotes_body(quotes_list)
 
 
 def show_random():
-    quotes_list = quotes.BashQuotes().get_random_quotes()
+    quotes_list = bash.get_random_quotes()
+    return get_quotes_body(quotes_list, random=True)
+
+
+def get_quotes_body(quotes_dict, random=False):
+    body = []
+    if not random:
+        for quote in sorted(quotes_dict, reverse=True):
+            text = ''
+            text += quote + '\n'
+            text += quotes_dict[quote] + '\n'
+            body.append(urwid.Text(text))
+            body.append(urwid.Divider())
+    else:
+        for quote in quotes_dict:
+            text = ''
+            text += quote + '\n'
+            text += quotes_dict[quote] + '\n'
+            body.append(urwid.Text(text))
+            body.append(urwid.Divider())
+    return body
+
+
+def show_next():
+    quotes_list = bash.get_next_page(cur_page)
     return get_quotes_body(quotes_list)
 
 
-def get_quotes_body(quotes_dict):
-    body = []
-    for quote in sorted(quotes_dict, reverse=True):
-        text = ''
-        text += quote + '\n'
-        text += quotes_dict[quote] + '\n'
-        body.append(urwid.Text(text))
-        body.append(urwid.Divider())
-    return body
+def show_prev():
+    quotes_list = bash.get_prev_page(cur_page)
+    return get_quotes_body(quotes_list)
 
 
 def item_chosen(button, choice):
     if choice == 'Новые':
         response = show_new()
-    elif choice == 'Случайные':
+    else: # choice == 'Случайные':
         response = show_random()
-    # response = urwid.Text([u'You chose ', choice, u'\n'])
+    show_page(response)
+
+
+def show_page(response, **kwargs):
+    kwargs['page'] = ''
     done = urwid.Button(u'Ok')
     response.append(urwid.AttrMap(done, None, focus_map='reversed'))
     urwid.connect_signal(done, 'click', exit_program)
     main.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(response))
-    # main.original_widget = urwid.Filler(urwid.Pile(response))
 
 
-def exit_program(button):
+def exit_program():
     raise urwid.ExitMainLoop()
 
 
@@ -60,6 +86,12 @@ def show_menu():
 def process_input(key):
     if key in ['TAB', 'tab']:
         show_menu()
+    elif key in ['q', 'Q']:
+        exit_program()
+    elif key in ['right', 'p']:
+        show_page(show_next())
+    elif key in ['left', 'o']:
+        show_page(show_prev())
 
 
 main = urwid.Padding(menu(u'Bash.im', choices), left=2, right=2)
--- main/quotes.py
+++ main/quotes.py
@@ -5,19 +5,23 @@ import re
 
 class BashQuotes:
     URL = 'bash.im'
-    g = Grab(url=URL)
+    g = Grab()
     g.go(URL)
+
+    quote_pattern = './/div[@class="quote"]'
 
     def _get_current_page(self):
         bash = self.g.doc.select('.//input[@class="page"]').html()
         page = re.findall('max="\d*"', bash)
         page = re.sub('\D', '', str(page))
-        print(page)
         return int(page)
 
     def _get_quotes_from_specified_page(self, page):
-        self.g.go(self.URL + '/index/' + str(page))
-        bash = self.g.doc.select('.//div[@class="quote"]')
+        if page == self.last_page:
+            self.g.go(self.URL + '/index/' + str(page))
+        else:
+            self.g.go(str(page))
+        bash = self.g.doc.select(self.quote_pattern)
         return self._get_quotes(bash)
 
     @staticmethod
@@ -43,29 +47,26 @@ class BashQuotes:
 
     def __init__(self):
         self.last_page = self._get_current_page()
-        self.pages_list = [k for k in range(self.last_page)]
+        self.pages_list = [k for k in range(self.last_page + 1)]
 
     def get_new_quotes(self):
         return self._get_quotes_from_specified_page(self.last_page)
 
     def get_next_page(self, page):
-        cur_index = self.pages_list.index(page)
         try:
-            page = self.pages_list[cur_index + 1]
+            page += 1
             return self._get_quotes_from_specified_page(page)
         except IndexError:
-            pass
+            return {}
 
     def get_prev_page(self, page):
-        cur_index = self.pages_list.index(page)
         try:
-            page = self.pages_list[cur_index - 1]
+            page -= 1
             return self._get_quotes_from_specified_page(page)
         except IndexError:
-            pass
+            return {}
 
     def get_random_quotes(self):
-        g = Grab(url=self.URL + '/random')
-        g.go("bash.im")
-        bash = g.doc.select('.//div[@class="quote"]')
+        self.g.go(self.URL + '/random')
+        bash = self.g.doc.select(self.quote_pattern)
         return self._get_quotes(bash)
